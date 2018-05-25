d2ac8f54eabce1afbbc4d462a46a756b2ba1ba67
Kevin Geng <khg@andrew.cmu.edu>
2018-05-24 23:38:16 -0400

    Add basic exam info page

--- registration/urls.py
+++ registration/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'registration'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('exams/<int:exam_id>/', views.exam_detail, name='exam-detail'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from .models import CourseUser
+from .models import CourseUser, Exam, ExamRegistration
 
 
 def index(request):
@@ -19,3 +19,15 @@ def index(request):
         return render(request, 'registration/auth_failure.html', {
             'remote_user': request.META.get('REMOTE_USER', None),
         })
+
+
+# TODO: Require authentication
+def exam_detail(request, exam_id):
+    exam = get_object_or_404(Exam, pk=exam_id)
+    exam_reg = get_object_or_404(ExamRegistration,
+            course_user__user=request.user.id, exam=exam)
+
+    return render(request, 'registration/exam_detail.html', {
+        'exam': exam,
+        'exam_reg': exam_reg,
+    })
