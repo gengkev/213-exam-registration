ad20b6bfa6a5740edfce9bd72566690f3f44e25d
Kevin Geng <khg@andrew.cmu.edu>
2018-05-26 00:09:01 -0400

    Improve admin interface

--- registration/admin.py
+++ registration/admin.py
@@ -3,10 +3,60 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Course, CourseUser, Room, Exam, TimeSlot, \
         ExamRegistration
 
+
+# Use default UserAdmin for User
 admin.site.register(User, UserAdmin)
-admin.site.register(Course)
-admin.site.register(CourseUser)
-admin.site.register(Room)
-admin.site.register(Exam)
-admin.site.register(TimeSlot)
-admin.site.register(ExamRegistration)
+
+
+# Declare inlines for later use
+
+class CourseUsersInstanceInline(admin.TabularInline):
+    model = CourseUser
+    extra = 0
+
+class ExamsInstanceInline(admin.TabularInline):
+    model = Exam
+    extra = 0
+
+class ExamRegistrationsInstanceInline(admin.TabularInline):
+    model = ExamRegistration
+    extra = 0
+
+class TimeSlotsInstanceInline(admin.TabularInline):
+    model = TimeSlot
+    extra = 0
+
+class RoomsInstanceInline(admin.TabularInline):
+    model = Room
+    extra = 0
+
+
+# Declare custom admins
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+    inlines = [
+        RoomsInstanceInline,
+        ExamsInstanceInline,
+        CourseUsersInstanceInline,
+    ]
+
+@admin.register(CourseUser)
+class CourseUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'course', 'user_type', 'lecture', 'section', 'dropped',
+    )
+    list_filter = ('course', 'user_type', 'dropped')
+    inlines = [
+        ExamRegistrationsInstanceInline,
+    ]
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('course', 'name')
+    list_filter = ('course',)
+    inlines = [
+        TimeSlotsInstanceInline,
+        ExamRegistrationsInstanceInline,
+    ]
