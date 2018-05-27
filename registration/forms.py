82a205d8f5898c0d1068fe0ee69f8cbc87c004b0
Kevin Geng <khg@andrew.cmu.edu>
2018-05-27 01:32:42 -0400

    Add basic timeslot selection

--- registration/forms.py
+++ registration/forms.py
@@ -1,9 +1,30 @@
 from django import forms
 
-from .models import User
+from .models import User, ExamRegistration, TimeSlot
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'timezone']
 
+
+class ExamRegistrationForm(forms.ModelForm):
+    time_slot = forms.ModelChoiceField(
+        queryset=None,
+        required=False,
+        widget=forms.RadioSelect,
+        empty_label="(Not registered)",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Filter possible time slots by exam
+        exam_reg = kwargs['instance']
+        self.fields['time_slot'].queryset = \
+            TimeSlot.objects.filter(exam=exam_reg.exam)
+
+    class Meta:
+        model = ExamRegistration
+        fields = ['time_slot']
--- registration/views.py
+++ registration/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, reverse
 from django.utils import timezone
 from django.views import generic
+from django.views.decorators.http import (
+    require_http_methods, require_safe, require_POST
+)
 
-from .forms import ProfileForm
+from .forms import ProfileForm, ExamRegistrationForm
 from .models import Course, CourseUser, Exam, ExamRegistration, User
 
 
+@require_safe
 @login_required
 def index(request):
     """
@@ -21,6 +25,7 @@ def index(request):
     })
 
 
+@require_http_methods(['GET', 'HEAD', 'POST'])
 @login_required
 def profile(request):
     user = get_object_or_404(User, pk=request.user.id)
@@ -44,26 +49,48 @@ def profile(request):
     })
 
 
+@require_safe
 @login_required
 def course_detail(request, course_code):
-    course = get_object_or_404(Course,
-            code=course_code)
-    course_user = get_object_or_404(CourseUser,
-            user=request.user.id, course=course)
+    course_user = get_object_or_404(
+        CourseUser,
+        user=request.user.id,
+        course__code=course_code
+    )
     return render(request, 'registration/course_detail.html', {
-        'course': course,
+        'course': course_user.course,
         'course_user': course_user,
     })
 
 
+@require_http_methods(['GET', 'HEAD', 'POST'])
 @login_required
 def exam_detail(request, course_code, exam_id):
-    exam = get_object_or_404(Exam,
-            pk=exam_id, course__code=course_code)
-    exam_reg = get_object_or_404(ExamRegistration,
-            course_user__user=request.user.id, exam=exam)
+    exam_reg = get_object_or_404(
+        ExamRegistration,
+        course_user__user=request.user.id,
+        exam=exam_id,
+        exam__course__code=course_code
+    )
+
+    if request.method == 'POST':
+        # Populate form with request data
+        form = ExamRegistrationForm(request.POST, instance=exam_reg)
+
+        # Check for validity
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse(
+                'registration:exam-detail',
+                args=[exam_reg.exam.course.code, exam_reg.exam.id],
+            ))
+
+    else:
+        # Create default form
+        form = ExamRegistrationForm(instance=exam_reg)
 
     return render(request, 'registration/exam_detail.html', {
-        'exam': exam,
+        'form': form,
+        'exam': exam_reg.exam,
         'exam_reg': exam_reg,
     })
