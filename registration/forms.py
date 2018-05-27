8b5e45576b6ac8e08f928a988579ab866b8f8024
Kevin Geng <khg@andrew.cmu.edu>
2018-05-27 16:47:17 -0400

    Add timezone selector

--- registration/forms.py
+++ registration/forms.py
@@ -1,12 +1,41 @@
+import pytz
+
 from django import forms
 
 from .models import User, ExamRegistration, TimeSlot
 
 
 class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Construct list of timezones
+        cmu_timezones = [('CMU Campuses', [
+            ('America/New_York', 'Pittsburgh (America/New_York)'),
+            ('Asia/Qatar', 'Qatar (Asia/Qatar)'),
+            ('America/Los_Angeles', 'Silicon Valley (America/Los_Angeles)'),
+        ])]
+
+        tz_timezones = [
+            (
+                f"{code}: {pytz.country_names[code]}",
+                [(tz, tz) for tz in tzlist],
+            )
+            for (code, tzlist) in pytz.country_timezones.items()
+        ]
+
+        # Apply to select field
+        self.fields['timezone'].widget.choices = (
+            cmu_timezones + tz_timezones
+        )
+
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'timezone']
+        widgets = {
+            'timezone': forms.Select(),
+        }
 
 
 class ExamRegistrationForm(forms.ModelForm):
