9235956e9ee08f31687388798dfcf52d23e2124f
Kevin Geng <khg@andrew.cmu.edu>
2018-08-08 05:32:38 -0400

    Add script to import usernames (todo: clean up)

---
+++ import_usernames.py
@@ -0,0 +1,24 @@
+import csv
+from registration.models import *
+
+course = Course.objects.get(code='15213-m18')
+exam = Exam.objects.get(course=course, name='Final Exam')
+print(course, exam)
+
+with open('usernames.txt', newline='') as f:
+    r = csv.reader(f)
+    for row in r:
+        username, password = row
+        try:
+            course_user = CourseUser.objects.get(
+                user__username=username,
+                course=course,
+            )
+        except CourseUser.DoesNotExist:
+            print('course user does not exist', username)
+        exam_reg, created = ExamRegistration.objects.get_or_create(
+            course_user=course_user,
+            exam=exam,
+        )
+        exam_reg.password = password
+        exam_reg.save()
